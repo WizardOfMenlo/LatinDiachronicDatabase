ad27f551d2065a180927dbefb68c9f70aa66ec09
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-04-03 15:58:24 +0100

    Added lemma mapping

--- src/word_db/src/lemmas.rs
+++ src/word_db/src/lemmas.rs
@@ -1,5 +1,8 @@
+use crate::forms::FormId;
+
 use interner::{impl_arena_id, RawId};
 use latin_utilities::NormalizedLatinString;
+use std::sync::Arc;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LemmaId(RawId);
@@ -7,3 +10,9 @@ impl_arena_id!(LemmaId);
 
 // Strong typedefs for more intuitive api
 pub type Lemma = NormalizedLatinString;
+
+#[salsa::query_group(LemmaQueryStorage)]
+trait LemmaQueryDatabase: salsa::Database {
+    #[salsa::input]
+    fn forms_for_lemma(&self, id: LemmaId) -> Arc<Vec<FormId>>;
+}
