8ba0833a6be712da23cb9d40ac3c3c318b183127
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-11-25 04:39:11 +0100

    Added export to csv

--- src/intersection/src/main.rs
+++ src/intersection/src/main.rs
@@ -1,8 +1,8 @@
+use authors_chrono::TimeSpan;
 use query_driver::driver_init;
 use query_system::lit_subset::LitSubset;
 use query_system::traits::*;
 use query_system::types::Author;
-use authors_chrono::TimeSpan;
 use runner::load_configuration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     color_backtrace::install();
     env_logger::init();
     let mut db = driver_init(load_configuration())?;
-    let lit = LitSubset::from_timespan(&TimeSpan::new_cent(-10, 1), db.authors(), &db.snapshot());
+    let lit = LitSubset::from_timespan(&TimeSpan::new_cent(-10, 0), db.authors(), &db.snapshot());
 
     let authors = LitSubset::from_authors(
         vec![
@@ -25,7 +25,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &db.snapshot(),
     );
 
-    let intersection : Vec<_> = db.intersect_sources(authors, lit).iter().map(|l| lemma_id_to_string(&db,*l)).collect();
+    let intersection: Vec<_> = db
+        .intersect_sources(authors, lit)
+        .iter()
+        .map(|l| lemma_id_to_string(&db, *l))
+        .collect();
 
     println!("{:?}", intersection);
 
