29e313001231975f4b43b8535e0c6c3f6a655670
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-01-17 07:49:47 +0100

    Removing garbage sweep for non garbageable stuff

--- src/query_driver/memory.rs
+++ src/query_driver/memory.rs
@@ -1,53 +1,23 @@
-use crate::query_system::{
-    middle::{
-        FormOccurrencesSubsetQuery, FormsInSourceQuery, FormsInSubsetQuery,
-        LemmaOccurrencesSubsetQuery, LemmasInSourceQuery, LemmasInSubsetQuery, ParseSubsetQuery,
-        SourceTreeQuery, SubsetTreeQuery,
-    },
-    sources::GetLineQuery,
-};
+use crate::query_system::sources::GetLineQuery;
 
 use super::MainDatabase;
 use crate::query_system::gc::GCollectable;
 
 use log::info;
-use salsa::{Database, Durability, SweepStrategy};
+use salsa::{Database, SweepStrategy};
 
 impl GCollectable for MainDatabase {
     fn garbage_sweep(&mut self) {
         info!("Sweeping garbage");
 
-        self.salsa_runtime_mut().synthetic_write(Durability::MEDIUM);
-
-        let sweep = SweepStrategy::discard_outdated();
+        let sweep = SweepStrategy::default().discard_values();
 
         self.query(GetLineQuery).sweep(sweep);
-        self.query(ParseSubsetQuery).sweep(sweep);
-        self.query(FormsInSubsetQuery).sweep(sweep);
-        self.query(FormsInSourceQuery).sweep(sweep);
-        self.query(LemmasInSubsetQuery).sweep(sweep);
-        self.query(LemmasInSourceQuery).sweep(sweep);
-        self.query(SourceTreeQuery).sweep(sweep);
-        self.query(SubsetTreeQuery).sweep(sweep);
-        self.query(FormOccurrencesSubsetQuery).sweep(sweep);
-        self.query(LemmaOccurrencesSubsetQuery).sweep(sweep);
     }
 }
 
 pub(super) fn set_lru_sizes(db: &mut MainDatabase) {
-    const FEW: usize = 32;
     const MANY: usize = 256;
 
     db.query_mut(GetLineQuery).set_lru_capacity(MANY);
-    db.query_mut(ParseSubsetQuery).set_lru_capacity(FEW);
-    db.query_mut(FormsInSubsetQuery).set_lru_capacity(FEW);
-    db.query_mut(FormsInSourceQuery).set_lru_capacity(FEW);
-    db.query_mut(LemmasInSubsetQuery).set_lru_capacity(FEW);
-    db.query_mut(LemmasInSourceQuery).set_lru_capacity(FEW);
-    db.query_mut(SourceTreeQuery).set_lru_capacity(FEW);
-    db.query_mut(SubsetTreeQuery).set_lru_capacity(FEW);
-    db.query_mut(FormOccurrencesSubsetQuery)
-        .set_lru_capacity(FEW);
-    db.query_mut(LemmaOccurrencesSubsetQuery)
-        .set_lru_capacity(FEW);
 }
